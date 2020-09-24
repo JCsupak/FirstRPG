66da7dc11f2e50a35bd704290188153edca0a867
James Csupak <jamescsupak@gmail.com>
2020-09-24 11:33:12 -0700

    Initial commit for RPG project

---
+++ Source/FirstRPG/FirstRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTRPG_API AFirstRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
